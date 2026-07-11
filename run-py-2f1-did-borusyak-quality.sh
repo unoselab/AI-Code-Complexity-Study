640eb1603f116f1d3d81e6bb1ac65885e97f2a0e
Song <song@example.com>
2026-07-11 02:29:38 -0500

    2026-07-11 02:29
    Updated

--- run-py-2f1-did-borusyak-quality.sh
+++ run-py-2f1-did-borusyak-quality.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-Rscript -e "rmarkdown::render('proc_r/DiffInDiffBorusyak.Rmd')"
-
+# Rscript -e "rmarkdown::render('proc_r/DiffInDiffBorusyak.Rmd')"
+Rscript -e "rmarkdown::render('proc_r/DiffInDiffBorusyak.Rmd', params = list(panel_file = '../repo_python/run-py-2e/strict/panel_event_monthly_quality_py.csv'))"
